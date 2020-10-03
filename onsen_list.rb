2ae44a400a912030aecc3aced7d9cffe5aa307e8
haverisxa <66747186+haverisxa@users.noreply.github.com>
2020-10-04 03:58:40 +0900

    Update onsen_list.rb

--- onsen_list.rb
+++ onsen_list.rb
@@ -4,7 +4,7 @@ require 'openssl'
 require 'kconv'
 require 'tk'
 
-ffmpeg = ""
+ffmpeg = "C:\\Programs\\ffmpeg-4.2.2-win64-static\\bin\\ffmpeg.exe"
 opt = "-vcodec copy -acodec copy -bsf:a aac_adtstoasc"
 
 def rkey(s, key)
@@ -21,14 +21,23 @@ end
 
 def date_tr(d)
     d_year = Time.now.strftime("%Y")
-    d_month = d.split("/")[0].rjust(2,"0")
-    d_day = d.split("/")[1].rjust(2,"0")
-    return "#{d_year}年#{d_month}月#{d_day}日 放送"
+    if(d == "null")
+        d = "0/0"
+    end
+    if(d)
+        d_month = d.split("/")[0].rjust(2,"0")
+        d_day = d.split("/")[1].rjust(2,"0")
+    else
+        d_month = "00"
+        d_day = "00"
+    end
+        return "#{d_year}年#{d_month}月#{d_day}日 放送"
 end
 
 url_n = "https://www.onsen.ag/web_api/programs"
 
 begin
+    puts "# Obtaining Program list ..."
     source_c = open(url_n, :ssl_verify_mode => OpenSSL::SSL::VERIFY_NONE).read
 rescue
     puts "#{url_n} open error."
@@ -40,6 +49,8 @@ title = ""
 b_num = ""
 b_date = ""
 b_list = []
+
+puts "# Making GUI ..."
 
 for s in source_c.split("{")
     ss = s.tosjis
@@ -53,10 +64,16 @@ for s in source_c.split("{")
     end
     if(ss.include?("updated"))
         b_date = rkey(ss, "updated")
+        #puts b_date
     end
     if(ss.include?("playlist.m3u8"))
         s_url = rkey(ss, "streaming_url")
-        b_list << ["#{b_date.split("/")[0].rjust(2,"0")}/#{b_date.split("/")[1].rjust(2,"0")}", "#{title} #{b_num} #{date_tr(b_date).tosjis}", s_url]
+        b_date = "0/0" if(b_date == "null")
+        b_date_m = b_date.split("/")[0]
+        b_date_d = b_date.split("/")[1]
+        b_date_m = "0" unless(b_date_m)
+        b_date_d = "0" unless(b_date_d)
+        b_list << ["#{b_date_m.rjust(2,"0")}/#{b_date_d.rjust(2,"0")}", "#{title} #{b_num} #{date_tr(b_date).tosjis}", s_url]
     end
 end
 
@@ -78,8 +95,9 @@ for list_line in b_list
 end
    
 TkButton.new(nil, text: '選択した番組をダウンロード', command: proc{(listbox.curselection).each{|i|
-    puts "Downloading: #{b_list[i][1]}"
+    puts "# Downloading: #{b_list[i][1]}"
     system("#{ffmpeg} -y -i \"#{b_list[i][2]}\" #{opt} \"#{b_list[i][1]}.mp4\"")
+    puts "# Download completed"
 }}).pack
 
 Tk.mainloop
